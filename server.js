1ec153dd384498f186bdf05c3de1c8215209a2a7
NicolasGerhardt <nicolas.gerhardt@gmail.com>
2018-09-01 13:35:08 -0400

    created base server

---
+++ server.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const port = 8000;
+
+
+console.log('== Starting Server ==');
+http.createServer(setupServer).listen(port);
+console.log('== Server Listening ==');
+
+
+function setupServer(req, res) {
+  let q = url.parse(req.url, true);
+  let filename = '.' + q.pathname;
+
+  console.log('Atempting to open: ' + filename);
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      console.log('Could not find: ' + filename);
+      res.qriteHead(404, {'Content-Type' : 'text/html'});
+      return res.end(`<html><body><h1>404 File Not Found! </h1></body></html>`);
+    }
+
+    console.log('Found file, loading: ' + filename);
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}
