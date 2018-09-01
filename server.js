70c593205f993916e536cc291b095ccf87d0b04e
NicolasGerhardt <nicolas.gerhardt@gmail.com>
2018-09-01 15:39:05 -0400

    full auto updating server

--- server.js
+++ server.js
@@ -7,40 +7,43 @@ const index = 'index.html';
 
 const port = 8000;
 
-console.log('Updating Index.html file');
+
 updateIndexHTML();
 
-// console.log('== Starting Server ==');
-// http.createServer(setupServer).listen(port);
-// console.log('== Server Listening ==');
+console.log('== Starting Server ==');
+http.createServer(setupServer).listen(port);
+console.log('== Server Listening ==');
 
 
-// function setupServer(req, res) {
-//   let q = url.parse(req.url, true);
-//   let filename = publicFolder + q.pathname;
+function setupServer(req, res) {
+  let q = url.parse(req.url, true);
+  let filename = publicFolder + q.pathname;
 
-//   if (filename == publicFolder +'/') {
-//     filename = publicFolder + '/index.html';
-//   }
+  if (filename == publicFolder +'/') {
+    updateIndexHTML();
+    filename = publicFolder + '/index.html';
+  }
 
-//   console.log('Atempting to open: ' + filename);
-//   fs.readFile(filename, (err, data) => {
-//     if (err) {
-//       console.log('Could not find: ' + filename);
-//       res.writeHead(404, {'Content-Type' : 'text/html'});
-//       return res.end(`<html><body><h1>404 File Not Found! </h1></body></html>`);
-//     }
+  console.log('Atempting to open: ' + filename);
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      console.log('Could not find: ' + filename);
+      res.writeHead(404, {'Content-Type' : 'text/html'});
+      return res.end(`<html><body><h1>404 File Not Found! </h1></body></html>`);
+    }
 
-//     console.log('Found file, loading: ' + filename);
-//     res.writeHead(200, {'Content-Type' : 'text/html'});
-//     res.write(data);
-//     return res.end();
-//   });
-// }
+    console.log('Found file, loading: ' + filename);
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}
 
 
 
 function updateIndexHTML() {
+  console.log('Updating Index.html file');
+
   let files = fs.readdirSync(publicFolder);
   files = filterOutArray(files, 'index.html');
   files = filterOutArray(files, '.DS_Store');
